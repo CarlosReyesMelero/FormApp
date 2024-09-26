639c01b28f63930923907a5c7b8193b8e2cd962e
CarlosReyesMelero <creyesmelero@gmail.com>
2024-09-26 21:26:28 +0200

    Validator function username admin

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { cantBeStrider } from '../../../shared/validators/validators.functions';
+
 @Component({
   templateUrl: './register-page.component.html',
 })
@@ -9,7 +11,7 @@ export class RegisterPageComponent {
   public myForm: FormGroup = this.fb.group({
     name: ['', [ Validators.required ]],
     email: ['', [ Validators.required ]],
-    username: ['', [ Validators.required ]],
+    username: ['', [ Validators.required, cantBeStrider ]],
     password: ['', [ Validators.required, Validators.minLength(6) ]],
     password2: ['', [ Validators.required, Validators.minLength(6) ]],
   });
